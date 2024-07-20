3356545320037796eb803868d6cbebac9bef35b8
Zayar Linn Naung <84829442+ZayarLinnNaung-Coder@users.noreply.github.com>
2024-07-20 22:52:37 +0630

    feat

--- src/main/java/thesis/zayarlinnhtet/volt_hunger/IMusicApplication.java
+++ src/main/java/thesis/zayarlinnhtet/volt_hunger/IMusicApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VoltHungerApplication {
+public class IMusicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VoltHungerApplication.class, args);
+		SpringApplication.run(IMusicApplication.class, args);
 	}
 
 }
